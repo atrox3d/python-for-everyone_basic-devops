91845f09eb12232cdaf0923253fe9f281e1f8915
atrox3d <atrox3d@gmail.com>
2025-03-05 10:47:14 +0100

    flake8

--- src/calculator/operations.py
+++ src/calculator/operations.py
@@ -1,7 +1,8 @@
-def add(a:int, b:int) -> int:
+def add(a: int, b: int) -> int:
     return a + b
 
-def subtract(a:int, b:int) -> int:
+
+def subtract(a: int, b: int) -> int:
     return a - b
 
 # def multiply(a:int, b:int) -> int:
